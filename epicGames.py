18599090abfd47c5ad450fd2171548254bd0a1c2
Karel Model <karel.model@hotmail.com>
2023-10-20 20:44:35 -0300

    Update epicGames.py
    Code adjustments and upgrade.

--- epicGames.py
+++ epicGames.py
@@ -8,28 +8,23 @@ locale.setlocale(locale.LC_ALL, 'pt_br.UTF-8')
 api = EpicGamesStoreAPI()
 promotions = api.get_free_games()
 
+date_format = '%Y-%m-%d'
+
 
 freeGames = []
-
-for game in promotions['data']['Catalog']['searchStore']['elements']:
-    if game['promotions'] is not None:        
-        if "promotionalOffers" in game['promotions']:
-            if game['promotions']['promotionalOffers']:
-                if(game['title'] != 'Mystery Game'):
-                    freeGames.append(game['title'])
-    
-#endDate = datetime.fromisoformat(game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['endDate'][:-1])
-#validPromotionMessage = "A promoção será válida somente até o dia " + datetime.strptime(str(endDate), '%Y-%m-%d %H:%M:%S').strftime('%d de %B')
-
-speak_output = "Os jogos que estão de graça hoje são: " + freeGames[0] + " e " + freeGames[1] + "."
-
-speak_output
-
 
 for game in promotions['data']['Catalog']['searchStore']['elements']:
     if game.get('promotions') != None:
         if game['promotions'].get('promotionalOffers') != None:
             if game['promotions']['promotionalOffers'] != []:
-                print(game['title'])
-                print(game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['startDate'])
-                print(game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['endDate'])
+                if(game['title'] != 'Mystery Game'):
+                    startDate=datetime.strptime(game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['startDate'][0:10], date_format)
+                    endDate=datetime.strptime(game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['endDate'][0:10], date_format)
+                    discountPercentage=game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['discountSetting']['discountPercentage']
+                
+                    if endDate > datetime.today() and discountPercentage == 0:
+                        freeGames.append(game['title'])
+    
+speak_output = "Os jogos que estão de graça hoje são: " + freeGames[0] + " e " + freeGames[1] + "."
+
+speak_output
